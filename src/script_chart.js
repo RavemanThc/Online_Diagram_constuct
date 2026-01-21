a1004a4d0a1c0b3c0dde4196b29ae30ba24381d1
Vlad.Harkusha <vlad.garkusha1999@gmail.com>
2026-01-21 20:26:08 +0200

    v2

--- src/script_chart.js
+++ src/script_chart.js
@@ -3,6 +3,7 @@ const valueInput = document.getElementById('valueInput');
 const colorInput = document.getElementById('colorInput');
 const chartType = document.getElementById('chartType');
 const zeroCheckbox = document.getElementById('zero');
+const items = document.getElementById('items');
 
 const ctx = document.getElementById('myChart');
 let chart;
@@ -20,10 +21,6 @@ function getRandomValue() {
   return Math.floor(Math.random() * 100) + 1;
 }
 
-function getRandomColor() {
-  return `hsl(${Math.random() * 360}, 70%, 60%)`;
-}
-
 function createChart(type) {
   if (chart) chart.destroy();
 
@@ -31,36 +28,47 @@ function createChart(type) {
     type,
     data: chartData,
     options: {
-      scales: type === 'pie' ? {} : {
-        y: { beginAtZero: true }
-      }
+      scales: ['pie','doughnut','polarArea'].includes(type) ? {} : { y: { beginAtZero: zeroCheckbox.checked } }
     }
   });
 }
 
 createChart('bar');
 
+// добавление данных
 document.getElementById('addBtn').addEventListener('click', () => {
   const label = labelInput.value.trim();
   if (!label) return;
 
-  const value = valueInput.value
-    ? Number(valueInput.value)
-    : getRandomValue();
+  let value = valueInput.value.trim();
+
+  if (!value) {
+    value = getRandomValue();
+  } else {
+    value = Number(value);
+    if (isNaN(value)) {;
+      return;
+    }
+  }
 
   chartData.labels.push(label);
   chartData.datasets[0].data.push(value);
-  chartData.datasets[0].backgroundColor.push(getRandomColor());
+  chartData.datasets[0].backgroundColor.push(colorInput.value);
 
+  renderList();
   chart.update();
 
   labelInput.value = '';
   valueInput.value = '';
 });
 
-document.getElementById('chartType').addEventListener('change', e => {
-  createChart(e.target.value);
-});
+// смена типа диаграммы
+chartType.addEventListener('change', e => createChart(e.target.value));
+
+// beginAtZero
+zeroCheckbox.addEventListener('change', () => createChart(chartType.value));
+
+// список с выбором цветов
 function renderList() {
   items.innerHTML = '';
 
@@ -74,7 +82,7 @@ function renderList() {
   });
 }
 
-
+// изменение цвета
 items.addEventListener('input', e => {
   if (e.target.type === 'color') {
     const index = e.target.dataset.index;
